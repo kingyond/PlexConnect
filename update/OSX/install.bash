ac5b4f699b87939639cbfbfbfeb250f63615cf77
stoffez <Admin@PlexServer.local>
2014-04-18 17:37:48 +0200

    Bash fixes for gradient installs

--- update/OSX/install.bash
+++ update/OSX/install.bash
@@ -7,17 +7,14 @@ cd /Applications/plexconnect/update/OSX/
 cp httpd.conf /etc/apache2
 
 ## copy gradient files
-cp -R GradienTV /library/webserver/documents
 cp -R GradientMovies /library/webserver/documents
+cp -R GradientTV /library/webserver/documents
+
 
 ## fix permissions
 chmod +x /Applications/plexconnect/update/OSX/websharing.bash
 chmod -R 0777 /library/webserver/documents/gradienttv/*.*
 chmod -R 0777 /library/webserver/documents/gradientmovies/*.*
-
-
-
-
 
 
 
